5daba19f7d40db708b7862602d21c93fbc2e77fa
Nacho Villanueva <ivillanueva@itba.edu.ar>
2023-05-13 16:45:02 -0300

    Better Glider Controller

--- ToyWars/Assets/Scripts/GliderController.cs
+++ ToyWars/Assets/Scripts/GliderController.cs
@@ -10,6 +10,13 @@ public class GliderController : MonoBehaviour
 
     public float speed = 10f; 
     
+    public float liftCoefficient = 0.1f; // Lift coefficient
+    public float dragCoefficient = 0.01f; // Drag coefficient
+    public float thrust = 1000f; // Thrust force
+    public float maxSpeed = 100f; // Maximum speed
+
+    private Rigidbody rb; // Reference to the Rigidbody component
+    
     private Vector2 _screenCenter;
 
     // Start is called before the first frame update
@@ -17,6 +24,7 @@ public class GliderController : MonoBehaviour
     {
         _screenCenter = new Vector2(Screen.width / 2, Screen.height / 2);
         Cursor.visible = false;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -27,6 +35,24 @@ public class GliderController : MonoBehaviour
         offset.y /= _screenCenter.y;
 
         transform.Rotate(offset.y, 0, -offset.x);
-        transform.position += transform.forward * (Time.deltaTime * speed);
+        // transform.position += transform.forward * (Time.deltaTime * speed);
+    }
+    
+    void FixedUpdate() {
+        // Calculate the forces of lift and drag
+        float speed = rb.velocity.magnitude;
+        float angleOfAttack = Vector3.Angle(rb.velocity, transform.forward) * Mathf.Deg2Rad;
+        Vector3 liftForce = 0.5f * liftCoefficient * speed * speed * angleOfAttack * transform.up;
+        Vector3 dragForce = -0.5f * dragCoefficient * speed * speed * rb.velocity.normalized;
+
+        // Apply the forces to the Rigidbody
+        rb.AddForce(transform.forward * thrust);
+        rb.AddForce(liftForce);
+        // rb.AddForce(dragForce);
+
+        // Limit the speed
+        if (rb.velocity.magnitude > maxSpeed) {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
     }
 }
