3a7c1c4b028274e8084a6482143b5b6a34427af5
Bensas <juanbensa@gmail.com>
2023-05-16 19:48:15 -0300

    Player movement now uses event queue, added moving powerup baloon

---
+++ ToyWars/Assets/Scripts/Commands/CmdBaloonMovement.cs
@@ -0,0 +1,19 @@
+﻿using Strategy;
+using UnityEngine;
+
+namespace Commands
+{
+    public class CmdBaloonMovement : ICommand
+    {
+        private IMoveableBaloon _moveable;
+        private Vector3 _direction;
+        
+        public CmdBaloonMovement(IMoveableBaloon moveable, Vector3 direction)
+        {
+            _moveable = moveable;
+            _direction = direction;
+        }
+        
+        public void Execute() => _moveable.Move(_direction);
+    }
+}
