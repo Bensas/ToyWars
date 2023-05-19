569516fcc8f7f74a43b975455cdb2afcba1c601c
Bensas <juanbensa@gmail.com>
2023-05-19 16:57:33 -0300

    Added damage to baloon

--- ToyWars/Assets/Scripts/Entities/Baloon.cs
+++ ToyWars/Assets/Scripts/Entities/Baloon.cs
@@ -3,31 +3,39 @@ using Commands;
 using Controllers;
 using Managers;
 using UnityEngine;
+using Strategy;
 
 namespace Entities
 {
-    public class Baloon : MonoBehaviour
+    public class Baloon : MonoBehaviour, IDamageable
     {
-        private BaloonMovementController _baloonMovementController;
+        // private BaloonMovementController _baloonMovementController;
 
         public float MAX_HEIGHT = 9;
         public float MIN_HEIGHT = 1;
+        public float SPEED = 0.0005f;
         public bool rising = true;
+        public float health = 10;
         
         public void Start()
         {
-            _baloonMovementController = GetComponent<BaloonMovementController>();
+            // _baloonMovementController = GetComponent<BaloonMovementController>();
             
         }
 
         void Update()
         {
-            Vector3 movementDirection = new Vector3(0, rising ? 1 : -1, 0);
+            transform.Translate(0, rising ? SPEED : -SPEED, 0);
             if (rising && transform.position.y > MAX_HEIGHT)
                     rising = false;
             else if (!rising && transform.position.y < MIN_HEIGHT)
                     rising = true;
-            GliderEventQueueManager.instance.AddEvent(new CmdBaloonMovement(_baloonMovementController, movementDirection));
+            
+        }
+
+        public void TakeDamage(float damage)
+        {
+            health -= damage;
         }
     }
 }
