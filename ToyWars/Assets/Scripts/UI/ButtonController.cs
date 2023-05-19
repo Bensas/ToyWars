db9044d63999f9dfd8c3fe7b442482c7343d14af
Nacho Villanueva <ivillanueva@itba.edu.ar>
2023-05-19 14:53:49 -0300

    Main menu + Sounds

---
+++ ToyWars/Assets/Scripts/UI/ButtonController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace UI
+{
+    [RequireComponent(typeof(Button))]
+    public class ButtonController : MonoBehaviour
+    {
+        private Button _button;
+        
+        public void OnSelect(BaseEventData eventData)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ ToyWars/Assets/Scripts/UI/UIButtonsLogic.cs
@@ -0,0 +1,16 @@
+﻿using Sound;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class UIButtonsLogic : MonoBehaviour
+    {
+        public void LoadMenuScene() => SceneManager.LoadScene("Main Menu");
+        public void LoadLevelScene() => SceneManager.LoadScene("BedroomScene");
+        public void CloseGame() => Application.Quit();
+
+        public void ButtonSelect() =>
+            SoundController.instace.PlaySound(SoundController.instace.SoundsLibrary.ButtonHighlight);
+    }
+}
