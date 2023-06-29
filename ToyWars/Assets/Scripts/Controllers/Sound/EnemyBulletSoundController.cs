a31d46ce19d59774c5ff04c41dc5514b1154bdce
Bensas <juanbensa@gmail.com>
2023-06-28 21:59:54 -0300

    Added sounds when a bullet hits the player

---
+++ ToyWars/Assets/Scripts/Controllers/Sound/EnemyBulletSoundController.cs
@@ -0,0 +1,42 @@
+using Flyweight;
+using Managers;
+using UnityEngine;
+
+namespace Sound
+{
+    public class EnemyBulletSoundController : SoundController
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+
+            _audioSource.clip = SoundsLibrary.BulletHit1;
+            _audioSource.loop = false;
+            Debug.Log(EventManager.instance);
+            
+            Invoke("AddFunctionToEventManager", 1);
+        }
+
+        private void AddFunctionToEventManager() {
+          EventManager.instance.OnPlayerHealthChange += OnBulletHitPlayer;
+        }
+
+        private void OnBulletHitPlayer(float health1, float health2)
+        {
+            Debug.Log("Playing bullet sound");
+            _audioSource.clip = GetRandomBulletHitSound();
+            Play();
+        }
+
+        private AudioClip GetRandomBulletHitSound() {
+          float rand = Random.value;
+          if (rand < 1f/3f) {
+            return SoundsLibrary.BulletHit1;
+          } else if (rand < 2f/3f) {
+            return SoundsLibrary.BulletHit2;
+          } else {
+            return SoundsLibrary.BulletHit3;
+          }
+        }
+    }
+}
